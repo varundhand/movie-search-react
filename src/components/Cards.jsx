2347102b13c9a2f7406d3a3e6549a1284ed19e4a
varundhand <varundhand77@gmail.com>
2023-04-24 01:26:54 +0530

    added like icons using vite-plugin-svgr

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,39 +1,39 @@
 import { useSelector, useDispatch } from "react-redux";
+import { ReactComponent as HeartSvg } from "../../public/heart.svg";
 
 const Cards = () => {
-
-  const movies = useSelector(state => state.movie.movies)
+  const movies = useSelector((state) => state.movie.movies);
   // const foundMovies = useSelector(state => state.movie.foundMovies)
-  const isLoading = useSelector((state) => state.movie.isLoading)
-  const error = useSelector((state) => state.movie.error)
-
+  const isLoading = useSelector((state) => state.movie.isLoading);
+  const error = useSelector((state) => state.movie.error);
 
   if (isLoading) {
-    return 'loading...'
+    return "loading...";
   }
 
   if (error) {
-    return error
+    return error;
   }
 
   // movies.forEach(({Poster,Title,Type,Year,imdbID}) => {
-
-  return ( 
-    <div style={{display:'flex'}}>
-      {movies.map(({Poster,Title,Type,Year,imdbID}) => {
-        return (<div className="card" style={{width:'11rem'}}>
-                <img src={Poster} className="card-img-top" />
-                <div className="card-body">
-                  <h5 className="card-title">{Title}</h5>
-                  <p className="card-text">{Year}</p>
-                  <p className="card-text">{Type}</p>
-              </div>
-      </div>)
-      })} 
+  return (
+    <div style={{ display: "flex" }}>
+      {movies.map(({ Poster, Title, Type, Year, imdbID }) => {
+        return (
+          <div className="card" style={{ width: "11rem" }} key={imdbID}>
+            <img src={Poster} className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">{Title}</h5>
+              <p className="card-text">{Year}</p>
+              <p className="d-flex justify-content-center cursor-pointer">
+                <HeartSvg />
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
-    );
-  
+  );
+};
 
-}
- 
 export default Cards;
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,34 +2,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies } from "../redux/movieSlice";
 
 const Pagination = () => {
-
-  const dispatch =useDispatch()
+  const dispatch = useDispatch();
 
   const searchTerm = useSelector((state) => state.movie.searchTerm);
-  const totalResults = useSelector(state => state.movie.totalResults)
-  const totalPages =  parseInt(totalResults/10)
-  console.log(totalPages)
-  const pages = Array.from(Array(totalPages).keys())
-  console.log(pages)
+  const totalResults = useSelector((state) => state.movie.totalResults);
+  const totalPages = parseInt(totalResults / 10);
+  console.log(totalPages);
+  const pages = Array.from(Array(totalPages).keys());
+  console.log(pages);
 
   const handlePageClick = (page) => {
-    console.log(page)
-    dispatch(fetchMovies({searchTerm,pageNo: page+1}))
-  }
+    console.log(page);
+    dispatch(fetchMovies({ searchTerm, pageNo: page + 1 }));
+  };
 
-  return ( 
+  return (
     <div className="paginator">
       <nav>
-        <ul class="pagination pagination-sm">
+        <ul className="pagination pagination-sm">
           {pages.map((page) => {
             return (
-              <li className="page-item"><a className="page-link" onClick={() => handlePageClick(page)} >{page + 1}</a></li>
-            )
+              <li key={page} className="page-item">
+                <a className="page-link" onClick={() => handlePageClick(page)}>
+                  {page + 1}
+                </a>
+              </li>
+            );
           })}
         </ul>
       </nav>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Pagination;
